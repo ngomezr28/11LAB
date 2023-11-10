b6503775e1db584b9d38430a725b6a0e6c9cdfbb
ngomezr28 <150452961+ngomezr28@users.noreply.github.com>
2023-11-10 10:03:46 -0500

    Update app.py

--- app.py
+++ app.py
@@ -1,7 +1,8 @@
 import streamlit as st
-
+from PIL import Image
 
 st.title("Mi primera app")
 
 st.header("Espacio para aprender a construir aplicaciones")
 st.write("Hub de Innovación SENA")
+image = Image.open('Propo.jpg')
