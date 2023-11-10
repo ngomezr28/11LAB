49af47874a8db1c5303de90409e3a87f1d6d1d78
ngomezr28 <150452961+ngomezr28@users.noreply.github.com>
2023-11-10 10:25:55 -0500

    Update app.py

--- app.py
+++ app.py
@@ -9,7 +9,7 @@ image = Image.open("propo.jpg")
 
 st.image(image, caption='Somos el Hub de Innovación')
 
-st.write("Equipo de trabajo especialziado")
+st.write("Equipo de trabajo especializado")
 image = Image.open("equipo.jpg")
 
 st.image(image, caption='Contamos con un equipo de profesionales expertos en innovación y tecnologías emergentes')
