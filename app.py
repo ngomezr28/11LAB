e0e3197dd0ae523ec18b37760f673d4eb6ff64e1
ngomezr28 <150452961+ngomezr28@users.noreply.github.com>
2023-11-10 10:17:33 -0500

    Update app.py

--- app.py
+++ app.py
@@ -7,5 +7,5 @@ st.header("Espacio para aprender a construir aplicaciones")
 st.write("Hub de Innovación SENA")
 image = Image.open("propo.jpg")
 
-
+st.image(image, caption='Somos el Hub de Innovación')
 
