36ec60f50e86a2a1514c511f494650a06e7f9614
ngomezr28 <150452961+ngomezr28@users.noreply.github.com>
2023-11-10 09:57:57 -0500

    Update app.py

--- app.py
+++ app.py
@@ -4,4 +4,4 @@ import streamlit as st
 st.title("Mi primera app")
 
 st.header("Espacio para aprender a construir aplicaciones")
-st.write("Hub de Innovación SENA)
+st.write("Hub de Innovación SENA")
